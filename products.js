1a6f7ddc91c36a384baca3c68844405736083962
santhosh62372n <126991013+santhosh62372n@users.noreply.github.com>
2023-05-12 14:35:05 -0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+
+router.get('/',(req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/',(req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId',(req, res, next) =>{
+    const id = req.params.productId;
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        res.status(200).json(doc);
+    }) 
+    .catch(err => { 
+        console.log(err);
+        res.status(500).json({error: err});
+    
+    });
+});
+
+router.patch('/:productId',(req, res, next) =>{
+    res.status(200).json({
+        message:'Updated Product!'
+    });
+});
+
+router.delete('/:productId',(req, res, next) =>{
+    res.status(200).json({
+        message:'Deleted Product!'
+    });
+});
+
+
+module.exports = router;
